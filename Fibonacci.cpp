3e0aacd74f02ba0eb210e62f8a90a7f49800e6b0
Daniel Kozhaev <106895453+nodlnodlnodl@users.noreply.github.com>
2023-02-22 22:02:51 +0300

    Create Fibonacci.cpp

---
+++ Fibonacci.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+
+int main()
+{
+    int a1 = 0;
+    int a2 = 1;
+    int n;
+
+    cout << "Enter number: ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        a1 = a1 + a2;
+        a2 = a1 - a2;
+    }
+    cout << a1 << endl;
+}
